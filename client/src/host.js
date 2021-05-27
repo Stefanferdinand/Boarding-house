a1d29fedcb359dc9cb7b04a265c3f21f977e934f
Stefan Ferdinand <stefan.ferdinand75@gmail.com>
2021-05-27 22:59:37 +0700

    add host

--- client/src/host.js
+++ client/src/host.js
@@ -1,7 +1,8 @@
 /*
-http://localhost:5000
+    HEROKU PRODUCTION = https://boarding-house-se.herokuapp.com
+    DEVELOPMENT = http://localhost:5000
 */
 
-const host = "http://localhost:5000";
+const host = "https://boarding-house-se.herokuapp.com";
 
 export default host;
