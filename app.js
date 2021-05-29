8b853a0686b9a124ff8c2f6d31cf8068df37dab3
Stefan Ferdinand <stefan.ferdinand75@gmail.com>
2021-05-29 22:56:22 +0700

    fix bugs

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use("/app", express.static(path.join(__dirname, "client", "build")));
 
 app.use("/auth", authRoute);
 app.use("/user", userRoute);
