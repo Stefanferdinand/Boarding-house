76ac94df50e2427270eaf63e003b2a3318ad5a8a
Stefan Ferdinand <stefan.ferdinand75@gmail.com>
2021-06-02 01:35:04 +0700

    fix bugs

--- client/src/Components/OwnedHouse.js
+++ client/src/Components/OwnedHouse.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import removeIcon from "../images/remove-icon.png";
+import host from "../host";
 
 function OwnedHouse(props) {
   return (
     <div className="owned-container">
       <div className="owned-image-container">
         <a href={`/house/${props.data._id}`}><img className="owned-image" src={props.data.image}></img></a>
-        <button type="button" className="btn btn-primary update-btn" onClick= {() => window.location.href = `/user/updateHouse/${props.data._id}`}>Update</button>
+        <button type="button" className="btn btn-primary update-btn" onClick= {() => window.location.href = `${host}/user/updateHouse/${props.data._id}`}>Update</button>
       </div>
       <span>{props.data.name}</span>
       <button className="remove-btn" type="button" onClick={() => props.remove(props.data._id)}>
