bd231f4968788194da382524d0347a342f2be5ad
Stefan Ferdinand <stefan.ferdinand75@gmail.com>
2021-06-03 01:52:30 +0700

    debugging

--- controllers/browse.js
+++ controllers/browse.js
@@ -3,7 +3,10 @@ const house = require("../models/house.js");
 const getBrowse = async (req, res) => {
   try {
     const arrayOfHouse = await house.find();
-    res.json(arrayOfHouse);
+    if(req.accepts('application/json')) res.json(arrayOfHouse);
+    else{
+      res.json({error: 'error'});
+    }
   } catch (error) {
     res.json(error);
   }
