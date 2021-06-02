1995219c9096074079d51d925879e47a2dec2eea
Stefan Ferdinand <stefan.ferdinand75@gmail.com>
2021-06-02 23:59:29 +0700

    debugging

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get('/*', (req, res, next) => {
   if(req.is('application/json'))res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  next();
 });
 
 app.use(express.json({ limit: "50mb" }));
