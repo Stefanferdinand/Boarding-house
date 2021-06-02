9076b237189b1eb15a3210bbb8a97762b44ae935
Stefan Ferdinand <stefan.ferdinand75@gmail.com>
2021-06-03 01:36:15 +0700

    debugging

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.use('/*', express.static(path.join(__dirname, "client", "build")));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 
 app.use("/auth", authRoute);
